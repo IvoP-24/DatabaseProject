7959d4779ca1fced065e85794a0e10362b3e8c52
IvoP-24 <pizzeriapineapple@gmail.com>
2025-10-29 13:44:03 +0200

    Enhance product management with command line support
    Refactor product management in main.c to support command line arguments for adding products and managing inventory. Added functions for saving, overwriting, and printing product data.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 struct Products{
     int product_id;
     char brand[50];
@@ -12,130 +13,162 @@ struct Products{
     char cpu[100];
     char gpu[100];
 };
-int main(){
 
-    int num_products;
-    
-    struct Products *productStatic = malloc(5 * sizeof(struct Products)); 
-    productStatic[0] = (struct Products){1, "Apple", "MacBook Pro", 999, 1399, "Laptop", 2022, 16, "Apple M2 Pro", "Integrated"};
-    productStatic[1] = (struct Products){2, "Dell", "XPS 15", 399, 1599, "Laptop", 2024, 32, "Intel Core i7-13700H", "NVIDIA RTX 4060"};
-    productStatic[2] = (struct Products){3, "HP", "Envy x360", 100, 599, "Laptop", 2021, 16, "AMD Ryzen 7 7840U", "Integrated"};
-    productStatic[3] = (struct Products){4, "Asus", "ROG Zephyrus G14", 500, 1399, "Laptop", 2025, 32, "AMD Ryzen 9 8945HS", "NVIDIA RTX 4070"};
-    productStatic[4] = (struct Products){5, "Microsoft", "Surface Laptop 7", 600, 1799, "Laptop", 2024, 16, "Snapdragon X Elite", "Adreno"};
-    
+int get_last_id(char *filename);
+void savefile(struct Products *products, int num_products, char *filename);
+void overwrite(struct Products *products, int num_products, char *filename);
+void printfile(char *filename);
 
-    for(int i = 0; i < 5; i++){
-        printf("\n-------------------");
-        printf("\n Unsorted array");
-        printf("\nProduct ID: %d\n", productStatic[i].product_id);
-        printf("Category: %s\n", productStatic[i].category);
-        printf("Brand: %s\n", productStatic[i].brand);
-        printf("Product Name: %s\n", productStatic[i].product_name);
-        printf("Year: %d\n", productStatic[i].year);
-        printf("RAM: %dGB\n", productStatic[i].ram);
-        printf("CPU: %s\n", productStatic[i].cpu);
-        printf("GPU: %s\n", productStatic[i].gpu);
-        printf("Quantity in stock: %d\n", productStatic[i].quantity);
-        printf("Price per unit: $%d\n", productStatic[i].per_unit);
+int main(int argc, char * argv[]){
+    struct Products *products = malloc(sizeof(struct Products));
+    int i = 1;
+    printf("--- Product Inventory Management ---\n");
+    if (argc >  1 && strcmp(argv[1], "--help") == 0) {
 
+        printf("Required 9 specifications : GPU = -g CPU = -c RAM = -r YEAR = -y BRAND = -b  QUANTITY = -q  PRICE PER UNIT = -p CATEGORY = -u PRODUCT NAME = -n\n");
+        printf("Delete everything = -o\n");
+        return-1;
+        free(products); }
+     else if (argc  == 1 ) {
+
+        printf("Invalid command. Type --help for more information.\n");
+        printf("\n--- Products ---\n");
+        printfile("products.txt");
+
+        return -1;
     }
+    if (argc > 1) {
 
-    struct Products temp;
-    for (int i = 0; i < 5 - 1; i++) {
-        for (int j = 0; j < 5 - i - 1; j++) {
-            if (productStatic[j].year > productStatic[j + 1].year) {
-                temp = productStatic[j];
-                productStatic[j] = productStatic[j + 1];
-                productStatic[j + 1] = temp;
+        struct Products product;
+        int count = 0;
+
+        for (int j = 1; j < argc; j++) {
+
+            if (strcmp(argv[j], "-g") == 0) {
+                strcpy(product.gpu, argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-c") == 0) {
+                strcpy(product.cpu, argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-r") == 0) {
+                product.ram = atoi(argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-y") == 0) {
+                product.year = atoi(argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-b") == 0) {
+                strcpy(product.brand, argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-n") == 0) {
+                strcpy(product.product_name, argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-q") == 0 ) {
+                product.quantity = atoi(argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-p") == 0) {
+                product.per_unit = atoi(argv[++j]);
+                count++;
+
+            } else if (strcmp(argv[j], "-u") == 0) {
+                strcpy(product.category, argv[++j]);
+                count++;
+
             }
+            else if (strcmp(argv[j], "-o") == 0) {
+            overwrite(products, 0, "products.txt");
+            printf("All products deleted.\n");
+        }
+        }
+        if(count == 9){
+
+            int last_id = get_last_id("products.txt");
+            product.product_id = get_last_id("products.txt") + 1; 
+            products[0] = product;
+            savefile(products, 1, "products.txt");
+            printf("\n--- Product Saved ---\n");
+            printfile("products.txt");
+            free(products);
+            return 0;
+
+        } 
+        else {
+            printf("Invalid or incomplete arguments. Type --help for more information.\n");
+            return -1;
         }
     }
 
-    for(int i = 0; i < 5; i++){
-        printf("\n-------------------");
-        printf("\n Sorted static array by year");
-        printf("\nProduct ID: %d\n", productStatic[i].product_id);
-        printf("Category: %s\n", productStatic[i].category);
-        printf("Brand: %s\n", productStatic[i].brand);
-        printf("Product Name: %s\n", productStatic[i].product_name);
-        printf("Year: %d\n", productStatic[i].year);
-        printf("RAM: %dGB\n", productStatic[i].ram);
-        printf("CPU: %s\n", productStatic[i].cpu);
-        printf("GPU: %s\n", productStatic[i].gpu);
-        printf("Quantity in stock: %d\n", productStatic[i].quantity);
-        printf("Price per unit: $%d\n", productStatic[i].per_unit);
+    free(products);
+    }   
 
+void overwrite(struct Products *products, int num_products, char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        return;
     }
-
-    printf("\nHow many products would you like to add:");
-    scanf("%d",&num_products);
-    struct Products *product = malloc(num_products * sizeof(struct Products));  
-    
-    for(int i = 0; i < num_products;i++){
-        product[i].product_id = i;
-        printf("\nEnter category:");
-        scanf("%s",product[i].category);
-        printf("\nEnter product name:");
-        scanf("%s",product[i].product_name);
-        printf("\nEnter product brand: ");
-        scanf("%s",product[i].brand);
-        printf("\nEnter year of manufacture:");
-        scanf("%d",&product[i].year);
-        printf("\nEnter product CPU:");
-        scanf("%s",product[i].cpu);
-        printf("\nEnter product GPU:");
-        scanf("%s",product[i].gpu);
-        printf("\nEnter product RAM in GB:");
-        scanf("%d",&product[i].ram);
-        printf("\nEnter quantity in stock:");
-        scanf("%d",&product[i].quantity);
-        printf("\nEnter price per unit:");
-        scanf("%d",&product[i].per_unit);
+    for (int i = 0; i < num_products; i++) {
+        fprintf(file, "%d,%s,%s,%s,%d,%d,%d,%s,%s,%d\n",
+                products[i].product_id,
+                products[i].category,
+                products[i].brand,
+                products[i].product_name,
+                products[i].year,
+                products[i].ram,
+                products[i].quantity,
+                products[i].cpu,
+                products[i].gpu,
+                products[i].per_unit);
     }
-    
-   
-    for(int i = 0; i < num_products; i++){
-        printf("\n-------------------");
-        printf("\nUnsorted dynamic array by year");
-        printf("\nProduct ID: %d\n", product[i].product_id);
-        printf("Category: %s\n", product[i].category);
-        printf("Brand: %s\n", product[i].brand);
-        printf("Product Name: %s\n", product[i].product_name);
-        printf("Year: %d\n", product[i].year);
-        printf("RAM: %dGB\n", product[i].ram);
-        printf("CPU: %s\n", product[i].cpu);
-        printf("GPU: %s\n", product[i].gpu);
-        printf("Quantity in stock: %d\n", product[i].quantity);
-        printf("Price per unit: $%d\n", product[i].per_unit);
-
-    }
-    struct Products tempdynamic;
-    for (int i = 0; i < num_products - 1; i++) {
-        for (int j = 0; j < num_products - i - 1; j++) {
-            if (product[j].year > product[j + 1].year) {
-                tempdynamic = product[j];
-                product[j] = product[j + 1];
-                product[j + 1] = tempdynamic;
-            }
-        }
-    }
-     
-    for(int i = 0; i < num_products; i++){
-        printf("\n-------------------");
-        printf("\nSorted dynamic array by year");
-        printf("\nProduct ID: %d\n", product[i].product_id);
-        printf("Category: %s\n", product[i].category);
-        printf("Brand: %s\n", product[i].brand);
-        printf("Product Name: %s\n", product[i].product_name);
-        printf("Year: %d\n", product[i].year);
-        printf("RAM: %dGB\n", product[i].ram);
-        printf("CPU: %s\n", product[i].cpu);
-        printf("GPU: %s\n", product[i].gpu);
-        printf("Quantity in stock: %d\n", product[i].quantity);
-        printf("Price per unit: $%d\n", product[i].per_unit);
-
-    }
-    free(product);
-    free(productStatic);
-return 0;
+    fclose(file);
 }
+void printfile(char *filename) {
+    FILE *file = fopen(filename, "r");
+    int c;
+    while ((c = fgetc(file)) != EOF) putchar(c);
+    fclose(file);
+}
+
+void savefile(struct Products *products, int num_products, char *filename) {
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) 
+        printf("The file is not opened.");
+    for (int i = 0; i < num_products; i++) {
+        fprintf(file, "%d,%s,%s,%s,%d,%d,%d,%s,%s,%d\n",
+                products[i].product_id,
+                products[i].category,
+                products[i].brand,
+                products[i].product_name,
+                products[i].year,
+                products[i].ram,
+                products[i].quantity,
+                products[i].cpu,
+                products[i].gpu,
+                products[i].per_unit);
+    }
+    fclose(file);
+}
+
+int get_last_id(char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        return 0; 
+    }
+    int last_id = 0;
+    char line[512];
+
+    while (fgets(line, sizeof(line), file)) {
+        int id;
+        if (sscanf(line, "%d,", &id) == 1 && id > last_id)
+            last_id = id;
+    }
+
+    fclose(file);
+    return last_id;
+}
+
