6489004d9d0aaa6cf03b17d8e2a99b5d0a44e793
IvoP-24 <pizzeriapineapple@gmail.com>
2025-12-17 09:52:10 +0200

    Removed string.h
    Refactor string handling functions and improve product sorting logic.

--- product.c
+++ product.c
@@ -1,13 +1,28 @@
 #include "product.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
+void str_copy(char *dest, char *src) {
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
 
+int str_compare(char *s1, char *s2) {
+    while (*s1 || *s2) {
+        if (*s1 != *s2) return *s1 - *s2;
+        s1++;
+        s2++;
+    }
+    return 0;
+}
 
 void sort_products(char *filename, char *criteria) {
-    int i,j;
+    int i, j;
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("No file found.\n");
@@ -16,35 +31,34 @@ void sort_products(char *filename, char *criteria) {
 
     struct Products products[1000];
     int count = 0;
-    char line[512];
-   while (fgets(line, sizeof(line), file)) {
+    char line[1000];
+    while (fgets(line, sizeof(line), file)) {
     if (sscanf(line, "%d,%[^,],%[^,],%[^,],%d,%d,%d,%[^,],%[^,],%d",
-               &products[count].product_id,
-               products[count].category,
-               products[count].brand,
-               products[count].product_name,
-               &products[count].year,
-               &products[count].ram,
-               &products[count].quantity,
-               products[count].cpu,
-               products[count].gpu,
-               &products[count].per_unit) == 10) {
-        count++;
+                   &products[count].product_id,
+                   products[count].category,
+                   products[count].brand,
+                   products[count].product_name,
+                   &products[count].year,
+                   &products[count].ram,
+                   &products[count].quantity,
+                   products[count].cpu,
+                   products[count].gpu,
+                   &products[count].per_unit) == 10) {
+            count++;
+        }
     }
-}
 
     fclose(file);
 
     for (i = 0; i < count - 1; i++) {
         for (j = 0; j < count - i - 1; j++) {
-            if (strcmp(criteria, "year") == 0) {
+            if (str_compare(criteria, "year") == 0) {
                 if (products[j].year > products[j + 1].year) {
                     struct Products temp = products[j];
                     products[j] = products[j + 1];
                     products[j + 1] = temp;
                 }
-            }
-            else if (strcmp(criteria, "price") == 0) {
+            } else if (str_compare(criteria, "price") == 0) {
                 if (products[j].per_unit > products[j + 1].per_unit) {
                     struct Products temp = products[j];
                     products[j] = products[j + 1];
@@ -53,18 +67,19 @@ void sort_products(char *filename, char *criteria) {
             }
         }
     }
+
     if (count == 0) {
-    printf("No products found to sort.\n");
-    return;
-}
+        printf("No products found to sort.\n");
+        return;
+    }
+
     overwrite(products, count, filename);
-    if (strcmp(criteria, "year") == 0) {
+
+    if (str_compare(criteria, "year") == 0) {
         printf("--- Products Sorted by Year ---\n");
-    }
-    else if (strcmp(criteria, "price") == 0) {
+    } else if (str_compare(criteria, "price") == 0) {
         printf("--- Products Sorted by Price ---\n");
-    }
-    else {
+    } else {
         printf("Invalid sort type. Use 'year' or 'price'.\n");
         return;
     }
@@ -84,12 +99,9 @@ void sort_products(char *filename, char *criteria) {
     }
 }
 
-
 void delete(struct Products *products, int num_products, char *filename) {
     FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        return;
-    }
+    if (file == NULL) return;
     for (int i = 0; i < num_products; i++) {
         fprintf(file, "%d,%s,%s,%s,%d,%d,%d,%s,%s,%d\n",
                 products[i].product_id,
@@ -105,16 +117,14 @@ void delete(struct Products *products, int num_products, char *filename) {
     }
     fclose(file);
 }
+
 void overwrite(struct Products *products, int num_products, char *filename) {
     if (num_products <= 0) {
         printf("File not modified, number of products is 0.\n");
         return;
     }
     FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        return;
-    }
-
+    if (file == NULL) return;
     for (int i = 0; i < num_products; i++) {
         fprintf(file, "%d,%s,%s,%s,%d,%d,%d,%s,%s,%d\n",
                 products[i].product_id,
@@ -130,8 +140,6 @@ void overwrite(struct Products *products, int num_products, char *filename) {
     }
     fclose(file);
 }
-
-
 
 void printfile(char *filename) {
     FILE *file = fopen(filename, "r");
@@ -140,12 +148,9 @@ void printfile(char *filename) {
     fclose(file);
 }
 
-
-
 void savefile(struct Products *products, int num_products, char *filename) {
     FILE *file = fopen(filename, "a");
-    if (file == NULL) 
-        printf("The file is not opened.");
+    if (file == NULL) printf("The file is not opened.");
     for (int i = 0; i < num_products; i++) {
         fprintf(file, "%d,%s,%s,%s,%d,%d,%d,%s,%s,%d\n",
                 products[i].product_id,
@@ -162,111 +167,86 @@ void savefile(struct Products *products, int num_products, char *filename) {
     fclose(file);
 }
 
-
-
 int get_last_id(char *filename) {
     FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
-        return 0; 
-    }
+    if (file == NULL) return 0; 
     int last_id = 0;
     char line[512];
-
     while (fgets(line, sizeof(line), file)) {
         int id;
         if (sscanf(line, "%d,", &id) == 1 && id > last_id)
             last_id = id;
     }
-
     fclose(file);
     return last_id;
 }
 
-
-
 int function(struct Products *products, char *filename, int argc, char *argv[]) {
     struct Products product;
-        int count = 0;
+    int count = 0;
     for (int j = 1; j < argc; j++) {
-
-            if (strcmp(argv[j], "-g") == 0) {
-                strcpy(product.gpu, argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-c") == 0) {
-                strcpy(product.cpu, argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-r") == 0) {
-                product.ram = atoi(argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-y") == 0) {
-                product.year = atoi(argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-b") == 0) {
-                strcpy(product.brand, argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-n") == 0) {
-                strcpy(product.product_name, argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-q") == 0 ) {
-                product.quantity = atoi(argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-p") == 0) {
-                product.per_unit = atoi(argv[++j]);
-                count++;
-
-            } else if (strcmp(argv[j], "-u") == 0) {
-                strcpy(product.category, argv[++j]);
-                count++;
-
-            }
-            else if (strcmp(argv[j], "-o") == 0) {
-                delete(products, 0, filename);
-                printf("All products deleted.\n");
-            }
-            else if (strcmp(argv[j], "--sort") == 0) {
-                if (j + 1 < argc) {
-                    j++;
-                    if (strcmp(argv[j], "price") == 0) {
-                        sort_products(filename, "price");
-                       
-                        return 0;
-                    } else if (strcmp(argv[j], "year") == 0) {
-                        sort_products(filename, "year");
-                     
-                        return 0;
-                    } else {
-                        printf("Unknown sort target: %s\n", argv[j]);
-                        return -1;
-                    }
+        if (str_compare(argv[j], "-g") == 0) {
+            str_copy(product.gpu, argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-c") == 0) {
+            str_copy(product.cpu, argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-r") == 0) {
+            product.ram = atoi(argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-y") == 0) {
+            product.year = atoi(argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-b") == 0) {
+            str_copy(product.brand, argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-n") == 0) {
+            str_copy(product.product_name, argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-q") == 0) {
+            product.quantity = atoi(argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-p") == 0) {
+            product.per_unit = atoi(argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-u") == 0) {
+            str_copy(product.category, argv[++j]);
+            count++;
+        } else if (str_compare(argv[j], "-o") == 0) {
+            delete(products, 0, filename);
+            printf("All products deleted.\n");
+        } else if (str_compare(argv[j], "--sort") == 0) {
+            if (j + 1 < argc) {
+                j++;
+                if (str_compare(argv[j], "price") == 0) {
+                    sort_products(filename, "price");
+                    return 0;
+                } else if (str_compare(argv[j], "year") == 0) {
+                    sort_products(filename, "year");
+                    return 0;
                 } else {
-                    printf("Invalid sort argument\n");
+                    printf("Unknown sort target: %s\n", argv[j]);
                     return -1;
                 }
+            } else {
+                printf("Invalid sort argument\n");
+                return -1;
             }
-            else if (strcmp(argv[j], "--print") == 0) {
-                printf("\n--- Products ---\n");
-                printfile(filename);
-            }
-        }
-        if(count == 9){
-
-            product.product_id = get_last_id(filename) + 1; 
-            products[0] = product;
-            savefile(products, 1, filename);
-            printf("\n--- Product Saved ---\n");
+        } else if (str_compare(argv[j], "--print") == 0) {
+            printf("\n--- Products ---\n");
             printfile(filename);
-
-        } 
-        else {
-            printf("Invalid or incomplete arguments. Type --help for more information.\n");
-            return -1;
         }
-        return 0;
+    }
+
+    if (count == 9) {
+        product.product_id = get_last_id(filename) + 1;
+        products[0] = product;
+        savefile(products, 1, filename);
+        printf("\n--- Product Saved ---\n");
+        printfile(filename);
+    } else {
+        printf("Invalid or incomplete arguments. Type --help for more information.\n");
+        return -1;
+    }
+    return 0;
 }
