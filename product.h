6ee541b8e07f6df4d6ccd874ef6e4327568ba510
IvoP-24 <pizzeriapineapple@gmail.com>
2026-03-14 17:44:49 +0200

    dynamic sizes remove stringh

--- product.h
+++ product.h
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 struct Products{
     int product_id;
-    char brand[50];
-    char product_name[100];
+    char *brand;
+    char *product_name;
     int quantity;
     int per_unit;
-    char category[100];
+    char *category;
     int year;
     int ram;
-    char cpu[100];
-    char gpu[100];
+    char *cpu;
+    char *gpu;
 };
 int function(struct Products *products, char *filename, int argc, char *argv[]);
 int get_last_id(char *filename);
 void savefile(struct Products *products, int num_products, char *filename);
-void overwrite(struct Products *products, int num_products, char *filename);
 void printfile(char *filename);
+void filter_products(char *filename, char *field, char *value);
 void sort_products(char *filename, char *criteria);
 void str_copy(char *dest, char *src);
 int str_compare(char *s1, char *s2);
